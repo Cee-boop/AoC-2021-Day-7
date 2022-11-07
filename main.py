a8859b8e04559448c950aeff396d1e1dc6a34360
Chantelle <79821347+Cee-boop@users.noreply.github.com>
2022-11-07 20:53:53 +1100

    Create main.py
    Part two is super slow but it works (around 20 seconds lol) I'm sick right now so I'll revise tomorrow.

---
+++ main.py
@@ -0,0 +1,38 @@
+with open(file='data.txt') as file:
+    data = sorted([int(num) for num in file.read().split(",")])
+
+
+def part_one():
+    max_num, min_num = data[-1], data[0]
+    lowest_fuel_cost = {'new_pos': 0, 'curr_lowest': float('inf')}
+    for new_pos in range(min_num, max_num + 1):
+        curr_fuel_cost = 0  # current iter fuel cost
+        for crab_pos in data:
+            curr_fuel_cost += abs(new_pos - crab_pos)
+
+        if curr_fuel_cost < lowest_fuel_cost['curr_lowest']:
+            lowest_fuel_cost['curr_lowest'] = curr_fuel_cost
+            lowest_fuel_cost['new_pos'] = new_pos
+
+    return print(lowest_fuel_cost['new_pos'])
+
+
+def part_two():
+    max_num, min_num = data[-1], data[0]
+    lowest_fuel_cost = {'new_pos': 0, 'curr_lowest': float('inf')}
+    for new_pos in range(min_num, max_num + 1):
+        curr_fuel_cost = 0  # current iter fuel cost
+        for crab_pos in data:
+            curr_fuel_cost += sum([i for i in range(1, abs(new_pos - crab_pos) + 1)])
+
+        if curr_fuel_cost < lowest_fuel_cost['curr_lowest']:
+            lowest_fuel_cost['curr_lowest'] = curr_fuel_cost
+            lowest_fuel_cost['new_pos'] = new_pos
+
+    return print(lowest_fuel_cost['new_pos'])
+
+
+# part one:
+part_one()
+# part two:
+part_two()
